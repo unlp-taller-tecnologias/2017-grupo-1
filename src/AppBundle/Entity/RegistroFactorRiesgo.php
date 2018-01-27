35814368ddbecac28d239d8401b6f8ab77edf24d
Nahuel <nahuel.zanelli@homail.com>
2018-01-27 10:05:55 -0300

    fix bug prod

--- src/AppBundle/Entity/RegistroFactorRiesgo.php
+++ src/AppBundle/Entity/RegistroFactorRiesgo.php
@@ -59,7 +59,7 @@ class RegistroFactorRiesgo {
         return $this->observacion;
     }
 
-    function getPropietario(): Visitante {
+    function getPropietario() {
         return $this->propietario;
     }
 
@@ -67,7 +67,7 @@ class RegistroFactorRiesgo {
         $this->propietario = $propietario;
     }
 
-    function getRegistroVacunacion(): RegistroVacunacion {
+    function getRegistroVacunacion() {
         return $this->registroVacunacion;
     }
 
